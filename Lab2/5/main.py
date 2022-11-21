089bc2ad577e0a6b243b5410aad4bc4927837d54
Armintas Pakenis <75906786+ArmintasP@users.noreply.github.com>
2022-11-21 11:21:32 -0500

    Add files via upload

--- Lab2/5/main.py
+++ Lab2/5/main.py
@@ -1,10 +1,18 @@
-from ete3 import Tree
+from ete3 import Tree, TreeStyle
+
 root = "MN514967.1-Dromedary-camel-coronavirus-HKU23-isolate-DcCoV-HKU23/camel/Nigeria/NV1385/2016"
 tree = Tree("/home/cat/Documents/Bioinformatika/Lab2/py/tree.out", format=1)
 
-filec = open("tree_not_outgrouped.txt", "w")
+ts = TreeStyle()
+ts.show_branch_length = True
+ts.scale = 6000
+ts.branch_vertical_margin = 10
 
+filec = open("tree_not_outgrouped.txt", "w")
 print(tree, file = filec)
+
+tree.render("tree_not_outgrouped.png", tree_style=ts)
+#tree.show(tree_style=ts)
 
 
 print("***")
@@ -13,4 +21,5 @@ filec = open("tree_outgrouped.txt", "w")
 tree.set_outgroup(root)
 print(tree, file=filec)
 
-tree.show(0)
+tree.render("tree_outgrouped.png", tree_style=ts)
+#tree.show(tree_style=ts)
